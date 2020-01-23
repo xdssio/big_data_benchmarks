d71fcc2f809d1c4b5688fc978bc1cc66309ddf62
Maarten Breddels <maartenbreddels@gmail.com>
2020-01-23 20:12:24 +0100

    make dask not compute arrauys and filters, and use the hdf5 file

--- notebooks/src/dask_utils.py
+++ notebooks/src/dask_utils.py
@@ -2,8 +2,18 @@ import dask.dataframe as dd
 import numpy as np
 
 
-def read_file(df=None, data_path=None):
+def read_file_parquet(df=None, data_path=None):
     return dd.read_parquet(data_path, engine='pyarrow')
+
+def read_file_vaex(df=None, data_path=None, chunksize='auto', column_names=None):
+    import vaex
+    data_path = '/data/yellow_taxi_2009_2015_f32.hdf5'
+    df = vdf = vaex.open(data_path)
+    column_names = column_names or vdf.get_column_names()
+    ddf = dd.from_array(vdf[column_names].to_dask_array(chunks=chunksize), columns=column_names)
+    # we manually add this column of a different type in
+    ddf['passenger_count'] = vdf['passenger_count'].to_dask_array(chunksize)
+    return ddf
     
 def mean(df):
     return df.fare_amount.mean().compute()
@@ -11,13 +21,13 @@ def mean(df):
 def standard_deviation(df):
     return df.fare_amount.std().compute()
 
-def sum_columns(df):
-    return dd.compute(df.fare_amount + df.trip_distance) 
+def mean_of_sum(df):
+    return (df.fare_amount + df.trip_distance).mean().compute()
 
-def product_columns(df):
-    return dd.compute(df.fare_amount * df.trip_distance)
+def mean_of_product(df):
+    return (df.fare_amount * df.trip_distance).mean().compute()
 
-def complicated_arithmetic_operation(df):
+def mean_of_complicated_arithmetic_operation(df):
     theta_1 = df.pickup_longitude
     phi_1 = df.pickup_latitude
     theta_2 = df.dropoff_longitude
@@ -25,7 +35,7 @@ def complicated_arithmetic_operation(df):
     temp = (np.sin((theta_2-theta_1)/2*np.pi/180)**2
            + np.cos(theta_1*np.pi/180)*np.cos(theta_2*np.pi/180) * np.sin((phi_2-phi_1)/2*np.pi/180)**2)
     ret = 2 * np.arctan2(np.sqrt(temp), np.sqrt(1-temp))
-    return dd.compute(ret)
+    return ret.mean().compute()
 
 def value_counts(df):
     return df.fare_amount.value_counts().compute()
@@ -49,5 +59,7 @@ def filter_data(df):
                   (df.pickup_latitude > lat_min)    & (df.pickup_latitude < lat_max) & \
                   (df.dropoff_longitude > long_min) & (df.dropoff_longitude < long_max) & \
                   (df.dropoff_latitude > lat_min)   & (df.dropoff_latitude < lat_max)
-    df[expr_filter].compute()
     return df[expr_filter]
+
+def length(df):
+    return len(df)
